7fe50c6c2fd4e050b373aafdcdf6f6163983f5b5
Romulo P T Mota <romuloptmota@gmail.com>
2026-02-27 10:14:18 -0300

    test: add user sorting by name scenario

---
+++ src/main/java/com/devsuperior/bds02/repository/CityRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.bds02.repository;
+
+import com.devsuperior.bds02.entities.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+    List<City> findAllByOrderByName();
+
+}
---
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.service;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    //FIND ALL SORTED BY NAME
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> cities = repository.findAllByOrderByName();
+        return cities.stream().map(CityDTO::new).toList();
+    }
+}
