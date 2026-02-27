43a79c36d2bf469d953ca19e4d2296121639018e
Romulo P T Mota <romuloptmota@gmail.com>
2026-02-27 14:26:14 -0300

    feat: implement event deletion with exception handling

---
+++ src/main/java/com/devsuperior/bds02/controller/EventController.java
@@ -0,0 +1,22 @@
+package com.devsuperior.bds02.controller;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.service.EventyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "events")
+public class EventController {
+
+    @Autowired
+    private EventyService service;
+
+    //UPDATE
+    @PutMapping(value = "/{id}")
+    public EventDTO update(@PathVariable Long id, @RequestBody EventDTO dto) {
+        EventDTO dtoUpdated = service.update(id, dto);
+        return dtoUpdated;
+    }
+
+}
---
+++ src/main/java/com/devsuperior/bds02/service/EventyService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds02.service;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repository.CityRepository;
+import com.devsuperior.bds02.repository.EventRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventyService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    //UPDATE
+    @Transactional
+    public EventDTO update(Long id, EventDTO dto) {
+
+        Event event = eventRepository.findById(id).orElseThrow(
+                ()-> new EntityNotFoundException("Entity event not found"));
+
+        event.setName(dto.getName());
+        event.setDate(dto.getDate());
+        event.setUrl(dto.getUrl());
+
+        City city = cityRepository.findById(dto.getCityId()).orElseThrow(
+                ()-> new EntityNotFoundException("Entity city not found"));
+
+        event.setCity(city);
+        eventRepository.save(event);
+        return new EventDTO(event);
+    }
+}
